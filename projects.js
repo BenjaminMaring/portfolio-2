4c4cf8d8a0d1fae7946c5c70c584332025911596
BenjaminMaring <benjammaring@gmail.com>
2023-11-03 18:30:10 -0700

    changed projects section entirely

---
+++ projects.js
@@ -0,0 +1,18 @@
+
+function select(choice) {
+    let splitness = document.getElementById('splitness');
+    let splitnessText = document.getElementById('splitness-text');
+
+    let links = document.getElementById('links');
+    let linksText = document.getElementById('links-text')
+
+if (choice == "splitness") {
+    splitness.classList = "item-wrapper white";
+    splitnessText.style = "display: block"
+
+
+}
+
+
+
+}
