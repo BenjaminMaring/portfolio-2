934aa58d5a5faffc67ea146fd1e911377fd48dfa
BenjaminMaring <benjammaring@gmail.com>
2023-11-03 22:21:21 -0700

    need to find a different way to display message

--- projects.js
+++ projects.js
@@ -4,13 +4,40 @@ function select(choice) {
     let splitnessText = document.getElementById('splitness-text');
 
     let links = document.getElementById('links');
-    let linksText = document.getElementById('links-text')
+    let linksText = document.getElementById('links-text');
+
+    let aerowings = document.getElementById('aerowings');
+    let aerowingsText = document.getElementById('aerowings-text');
+
+    SVGFESpotLightElement
 
 if (choice == "splitness") {
     splitness.classList = "item-wrapper white";
-    splitnessText.style = "display: block"
+    splitnessText.style = "display: block;"
 
+    links.classList = "item-wrapper unselected";
+    linksText.style = "display: none;";
 
+    aerowings.classList = "item-wrapper unselected";
+    aerowingsText.style = "display: none;";
+} else if (choice == "links") {
+    splitness.classList = "item-wrapper unselected";
+    splitnessText.style = "display: none;"
+
+    links.classList = "item-wrapper white";
+    linksText.style = "display: block;"
+
+    aerowings.classList = "item-wrapper unselected";
+    aerowingsText.style = "display: none;";
+} else if (choice == "aerowings") {
+    splitness.classList = "item-wrapper unselected";
+    splitnessText.style = "display: none;"
+
+    links.classList = "item-wrapper unselected";
+    linksText.style = "display: none;";
+
+    aerowings.classList = "item-wrapper white";
+    aerowingsText.style = "display: block;";
 }
 
 
